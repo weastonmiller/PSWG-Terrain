703a4ebac486594b164eff9cf1123c878bab0ae3
weastonmiller <westonmiller@yahoo.com>
2019-03-19 20:40:07 -0400

    baseline wastes

--- tatooine/wastes.cs
+++ tatooine/wastes.cs
@@ -15,16 +15,21 @@ class TerrainGenerator
     /// <returns>A height between 0 and 255, inclusive</returns>
     public double GetTerrain(double x, double z)
     {
-        var j = ProcNoise.OctaveNoise(x / 110, z / 110, 4);
+        var j = ProcNoise.OctaveNoise(x / 420, z / 420, 4);
 		
 		// the first 2 values essentially translate to the radius of the creaters, smaller the number the bigger the creater
-		var h = ProcNoise.Worley(x / 400, z / 400, j) * 1.45;
+		// var h = ProcNoise.Worley(x / 400 + 200, z / 400 + 200, j) * 1.45;
+        var h1 = ProcNoise.OctaveNoise(x / 110, z / 110, 4);
+        var h2 = ProcNoise.OctaveNoise(x / 110 + 999, z / 110 - 999, 4);
+        
+        // the first 2 values essentially translate to the radius of the creaters, smaller the number the bigger the creater
+        var h = ProcNoise.Worley(x / 400 + h1, z / 400 - h1, j) * 1.3;
 		if (h < 1)
 			h = Circularize(h);
 		else
 			h = 1;
 		
-        return h * 100 + ProcNoise.OctaveNoise(x / 70, z / 70, 5) * 40;
+        return h * 80 + ProcNoise.OctaveNoise(x / 70, z / 70, 5) * 20;
     }
 	
 	private double Circularize(double x)
